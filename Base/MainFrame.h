b312872701f9218cc84447852fab5c281966b40c
pav007prikhod <pav007prikhod@mail.ru>
2018-05-14 15:41:55 +0300

    Добавьте файлы проекта.

---
+++ Base/MainFrame.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace Base {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MainFrame : public System::Windows::Forms::Form
+	{
+	public:
+		MainFrame(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MainFrame()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Main frame
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(651, 411);
+			this->Name = L"MainFrame";
+			this->Text = L"MainFrame";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
---
+++ Base/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MainFrame.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Base::MainFrame form;
+	Application::Run(%form);
+}
